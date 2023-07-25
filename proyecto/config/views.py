8b6085817be788e089c7eefc40af86a4eddc4959
Rodrigar <rodriadriangarcia1997@gmail.com>
2023-07-25 20:07:23 -0300

    1

--- proyecto/config/views.py
+++ proyecto/config/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 def mis_notas(request):
     lista_de_notas = [2,3,4,5,6,7,8,9]
-    context = {"notas" : lista_de_notas}
-    return render(request,"index.html" , context)
+    contexto = {"notas" : lista_de_notas}
+    return render(request,"templates/index.html" , contexto)
